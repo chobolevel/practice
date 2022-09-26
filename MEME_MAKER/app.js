5372234a65c5c930eacd4a5f25cf7149ecfb49c6
chobolevel <104749958+chobolevel@users.noreply.github.com>
2022-09-26 23:29:13 +0900

    MEME_MAKER기능 완성

--- MEME_MAKER/app.js
+++ MEME_MAKER/app.js
@@ -1,12 +1,27 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 //ctx는 canvas에서 쓰일 브러쉬임
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+const saveBtn = document.getElementById("save");
 
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = 2;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+//선의 끝을 둥글게 만드는 옵션
 
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
     if(isPainting === true) {
@@ -18,16 +33,96 @@ function onMove(event) {
         ctx.moveTo(event.offsetX, event.offsetY);
     }
 }
-
 function startPainting() {
     isPainting = true;
 }
-
 function cancelPainting() {
     isPainting = false;
+    ctx.beginPath();
+}
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+function onCanvasClick() {
+    if(isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+function onDestroyClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.value = "Fill";
+}
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
+function onDoubleClick(event) {
+    const text = textInput.value;
+    if(text !== "") {
+        ctx.save();
+        //현재 상태, 색상, 스타일 등 모든 것을 저장함
+        ctx.lineWidth = 1;
+        //기본 lineWidth가 5로 되어 있어서 변경하고 text를 canvas에 추가해야함
+        ctx.font = "68px serif";
+        //font는 사이즈와 폰트 스타일만 지정가능함
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        //fill stroke차이는 글자 내부를 채우느냐 안채우느냐의 차이
+        ctx.restore();
+        //restore함수로 원상복구
+    }
+    //text값이 없을 경우에는 작동하지 않도록 만듦
+    
+}
+function onSaveClick() {
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click();
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+canvas.addEventListener("dblclick", onDoubleClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
